9beea5414d6e2c829060cd3a414070405d2dfafe
Roman Lukachyk <petrovroman26@icloud.com>
2023-01-29 22:13:55 +0200

    add Files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './index.scss';
+import React from "react";
+import { Users } from "./components/Users";
+import { Success } from "./components/Success";
+import "./index.scss";
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      {/* <Users /> */}
+
+      <Success />
     </div>
   );
 }
